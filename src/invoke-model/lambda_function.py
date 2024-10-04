e22f71e1b969c5b854eada4c84fea41d327a7599
PARISOT Cyril <cyparisot@SFT-PAR-MBP3514.local>
2024-10-04 10:52:54 +0200

    init template

---
+++ src/invoke-model/lambda_function.py
@@ -0,0 +1,42 @@
+import json
+import boto3 
+
+modelId = "anthropic.claude-3-5-sonnet-20240620-v1:0"
+
+def invoke_bedrock(prompt_data):
+    body = json.dumps({
+        'prompt': f'Human:{prompt_data}\n\nAssistant:', 
+        'max_tokens_to_sample': 1028,
+        'temperature': 1,
+        'top_k': 250,
+        'top_p': 0.999,
+        'stop_sequences': ['\n\nHuman:']
+    })
+
+    client = boto3.client('bedrock-runtime')
+    response = client.invoke_model(
+        body=body, modelId=modelId, contentType="application/json",
+        accept="*/*"
+    )
+    return json.loads(response["body"].read().decode("utf-8"))
+
+def lambda_handler(event, context):
+    responses = []
+    for record in event['Records']:
+        print(record)
+        request = json.loads(record["body"])
+        print(request)
+        prompt_data = request["prompt"]
+        
+        response = invoke_bedrock(prompt_data)
+        responses.append(response)
+        print(response)
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps(responses)
+    }
+
+        #### Publish response to websocket, IoT Core topic
+
+
